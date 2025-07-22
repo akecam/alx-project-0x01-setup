6de6bab026bc341c68b845fe295af52cf2c18d04
Donald Umeh <donaldumeh54@gmail.com>
2025-07-22 22:16:24 +0100

    implement users card components

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,88 @@
+import { Address, Company, Geo, UserProps } from "@/interfaces";
+import Image from "next/image";
+import profile_avatar from "@/public/assets/images/profile_avatar.svg";
+
+const UserCard: React.FC<UserProps> = ({
+  id,
+  name,
+  username,
+  email,
+  address,
+  phone,
+  website,
+  company,
+}) => {
+  return (
+    <section className="max-w-md mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-col justify-center items-center hover:border-x-amber-200">
+      <div className="rounded-full bg-amber-100">
+        <Image src={profile_avatar} alt="avatar" width={150} height={150} />
+      </div>
+
+      <div>
+        <p>
+          <b>ID:</b> {id}
+        </p>
+        <p>
+          <b>Name:</b> {name}
+        </p>
+        <p>
+          <b>Username:</b> {username}
+        </p>
+        <p>
+          <b>Email:</b> {email}
+        </p>
+
+        <div>
+          <p>
+            <b>Address:</b>
+          </p>
+          <p>
+            <b>Street:</b> {address?.street}
+          </p>
+          <p>
+            <b>Suite:</b> {address?.suite}
+          </p>
+          <p>
+            <b>City:</b> {address?.city}
+          </p>
+          <p>
+            <b>Zipcode:</b> {address?.zipcode}
+          </p>
+          <p>
+            <b>Geo:</b>
+          </p>
+          <p>
+            <b>Latitude:</b> {address?.geo?.lat}
+          </p>
+          <p>
+            <b>Longitude:</b> {address?.geo?.lng}
+          </p>
+        </div>
+
+        <p>
+          <b>Phone:</b> {phone}
+        </p>
+        <p>
+          <b>Website:</b> {website}
+        </p>
+
+        <div>
+          <p>
+            <b>Company:</b>
+          </p>
+          <p>
+            <b>Company Name:</b> {company?.name}
+          </p>
+          <p>
+            <b>CatchPhrase:</b> {company?.catchPhrase}
+          </p>
+          <p>
+            <b>BS:</b> {company?.bs}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default UserCard;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,12 +1,49 @@
 import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
 
-const Users = () => {
+const Users: React.FC<UserProps[]> = ({ users }) => {
   return (
     <div>
       <Header />
-      <p>Users</p>
+
+      {users?.map(
+        ({
+          id,
+          name,
+          username,
+          email,
+          address,
+          phone,
+          website,
+          company,
+        }: UserProps) => (
+          <UserCard
+            name={name}
+            key={id}
+            id={id}
+            username={username}
+            email={email}
+            address={address}
+            phone={phone}
+            website={website}
+            company={company}
+          />
+        )
+      )}
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+}
 
 export default Users;
