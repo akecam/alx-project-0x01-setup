066c9f45f0e830e322ba8d9d5988a3ca2fcd23d5
Donald Umeh <donaldumeh54@gmail.com>
2025-07-22 22:18:10 +0100

    implement users card components

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,13 +1,14 @@
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
+import posts from "../posts";
 
-const Users: React.FC<UserProps[]> = ({ users }) => {
+const Users: React.FC<UserProps[]> = ({ posts }) => {
   return (
     <div>
       <Header />
 
-      {users?.map(
+      {posts?.map(
         ({
           id,
           name,
@@ -37,11 +38,11 @@ const Users: React.FC<UserProps[]> = ({ users }) => {
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
+  const posts = await response.json();
 
   return {
     props: {
-      users,
+      posts,
     },
   };
 }
